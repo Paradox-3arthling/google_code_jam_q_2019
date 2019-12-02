19cb203244c145b078e174636b48d7d851f2abea
savage_floweed <FloydQaranja@gmail.com>
2019-12-02 13:20:36 +0300

    Time to test it!

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use std::io;
 
 fn main() {
-    println!("Insert value");
-   let val: u32 = integer_input();
-   println!("value input {}", val);
-   for i in 1..val - 1 {
-       if i % 2 != 0 {
-          continue;
-       }
-       let val_1: String = i.to_string();
-       let val_2: u32 = val - i;
-       let val_2 = val_2.to_string();
-       if val_1.contains("4") || val_2.contains("4") {
-         continue;
-       }
-       let last_val: u32 = i + (val - i);
-       if last_val == val {
-         println!("{}, {}", i, val - i); 
-         break;  
-       }
+   let tries: u32 = integer_input();
+
+   for x in 0..tries {
+    let val: u32 = integer_input();
+    for i in 1..val - 1 {
+        if i % 2 != 0 {
+            continue;
+        }
+        let val_1: String = i.to_string();
+        let val_2: u32 = val - i;
+        let val_2 = val_2.to_string();
+        if val_1.contains("4") || val_2.contains("4") {
+            continue;
+        }
+        let last_val: u32 = i + (val - i);
+        if last_val == val {
+            println!("Case #{}: {} {}", x + 1, i, val - i); 
+            break;  
+        }
+    }
    }
 }
 fn integer_input() -> u32 {
