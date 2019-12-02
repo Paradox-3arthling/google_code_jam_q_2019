528ea885374d54f97a41d8624a59e44c18cdc69a
savage_floweed <FloydQaranja@gmail.com>
2019-12-02 12:47:47 +0300

    Number of test cases coming in

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Insert value");
+   let val: u32 = integer_input();
+   println!("value input {}", val);
+   println!("value {}", u32::max_value());
+   for i in 1..val + 1 {
+       println!("value {}", i);
+   }
 }
+fn integer_input() -> u32 {
+    let mut val: String = String::new();
+    io::stdin().read_line(&mut val)
+        .expect("Failed to read input!");
+    let val: u32 = val.trim().parse()
+        .expect("Expected integer!");
+    val
+}
+
